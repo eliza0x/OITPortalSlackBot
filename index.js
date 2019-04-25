d3a3365e9d11fa15304c2540976c72b657765c54
Sohei Yamaga <me@eliza.link>
2019-04-25 19:32:17 +0900

    差分のみを通知するように

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 const puppeteer = require('puppeteer');
 const req = require('request');
+const fs = require('fs');
+
+function readLog() {
+  return JSON.parse(fs.readFileSync('log.json', 'utf8'))
+}
+
+function writeLog(obj) {
+  return fs.writeFileSync('log.json', JSON.stringify(obj))
+}
+
+function postSlack(text) {
+    const options = httpOption(text)
+    req.post(options, function(error, response, body){});
+}
 
 function httpOption(text) {
   return {
@@ -28,11 +42,8 @@ function httpOption(text) {
     await page.type('#userId', process.env.OIT_USERNAME);
     await page.type('#password', process.env.OIT_PASSWD);
     await page.click('#loginButton');
-  
-    // let my_notices_html = await page.$$('.inner')[2];
-    // let college_html = await page.$$('.inner')[3];
-  
-    await page.waitFor(1000);
+    await page.waitFor(10000);
+
     const personal_inf = await page.evaluate(() => {
       let html = document.querySelector("div.personal_inf");
       html = Array.from(html.querySelectorAll("div.details"));
@@ -54,25 +65,48 @@ function httpOption(text) {
     });
     browser.close()
   
-    let text = ""
-    text += "個人向けの連絡(eliza0x)\n"
-    text += "==========\n"
-    personal_inf.slice(0,5).forEach(e => text+=e+"\n")
-    text += "\n"
-    text += "全体向けの連絡\n"
-    text += "==========\n"
-    public_inf.slice(0,5).forEach(e => text+=e+"\n")
-    text += "\n"
-    text += (new Date()).toString()
-    
-    var options = httpOption(text)
-    req.post(options, function(error, response, body){});
+    const log = readLog()
+    let new_personal_inf = []
+    let new_public_inf = []
+
+    personal_inf.forEach (e => {
+      let new_arrayvals = true
+      log.personal_inf.forEach (le => {
+        if (e == le) new_arrayvals = false
+      })
+      if (new_arrayvals) new_personal_inf.push(e)
+    })
+
+    public_inf.forEach (e => {
+      let new_arrayvals = true
+      log.public_inf.forEach (le => {
+        if (e == le) new_arrayvals = false
+      })
+      if (new_arrayvals) new_public_inf.push(e)
+    })
+
+    writeLog({personal_inf: personal_inf, public_inf: public_inf})
+
+
+    if (new_personal_inf.length > 0 || new_public_inf.length > 0) {
+      let text = "新しい通知を検知:\n"
+      if (new_personal_inf.length >= 0) {
+        text += "個人向けの連絡(eliza0x)\n"
+        text += "==========\n"
+        new_personal_inf.forEach(e => text+=e+"\n")
+        text += "\n"
+      }
+      if (new_public_inf.length >= 0) {
+        text += "全体向けの連絡\n"
+        text += "==========\n"
+        new_public_inf.forEach(e => text+=e+"\n")
+        text += "\n"
+      }
+      postSlack(text)
+    }
   } catch (e) {
-    
-    var options = httpOption("情報の取得に失敗\n==========\n" + e.toString())
-    req.post(options, function(error, response, body){});
+    postSlack("情報の取得に失敗\n==========\n" + e.toString())
   }
 })();
-
 
 // curl -X POST --data-urlencode 'payload={"username": "PortalBot", "text": "いまbot作ってる"}' https://hooks.slack.com/services/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
